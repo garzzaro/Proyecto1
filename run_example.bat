02ceeb7c422d9778ec70db747e07dfe11e9b857f
garzzaro <barbaragarzaro06@gmail.com>
2026-03-20 08:25:57 -0600

    CAMBIOS

--- run_example.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Compiling...
-call mvn compile
-echo.
-echo Running Example Script: 1 2 OP_ADD 5 OP_GREATERTHAN
-echo.
-java -cp "target\classes" com.bitcoin.Main --trace "1 2 OP_ADD 5 OP_GREATERTHAN"
-echo.
-pause
